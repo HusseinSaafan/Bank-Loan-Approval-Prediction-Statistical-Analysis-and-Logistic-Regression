253920ec285ee7a7d4191929a8cbb13b3d3aafd0
Saafan <saafan@Saafans-Laptop.local>
2026-04-02 21:15:08 +0200

    2_4: pushing main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from src.utils.config import logger
 def main():
     logger.info("Starting the Bank Loan Approval Prediction project.")
     # Your main code for data loading, preprocessing, model training, etc. goes here.
+    # For example: ingestion, preprocessing, model training, evaluation, etc.
     logger.info("Finished processing.")
 if __name__ == "__main__":
     main()
