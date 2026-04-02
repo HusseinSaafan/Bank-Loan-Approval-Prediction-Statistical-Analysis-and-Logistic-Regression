15f05b60c5bf69258178bc8228b0157f25acf978
Saafan <saafan@Saafans-Laptop.local>
2026-04-02 21:07:02 +0200

    2_4: setup project

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from src.utils.config import logger
+def main():
+    logger.info("Starting the Bank Loan Approval Prediction project.")
+    # Your main code for data loading, preprocessing, model training, etc. goes here.
+    logger.info("Finished processing.")
+if __name__ == "__main__":
+    main()
